2338b64b67160801a7eea48bd99be1cc0913faa7
Le Ngoc Linh <lnlinh93@dinsight.ai>
2024-10-11 20:05:11 +0900

    remove the event sender, fix undistortion

--- lidar.py
+++ lidar.py
@@ -2,6 +2,7 @@ import os
 import json
 from typing import Optional
 import glob
+import copy
 
 import carb
 import omni.graph.core as og
@@ -11,18 +12,20 @@ from omni.isaac.core import SimulationContext
 from omni.isaac.core.utils.prims import *
 from omni.isaac.IsaacSensorSchema import IsaacRtxLidarSensorAPI
 import omni.replicator.core as rep
+import omni.syntheticdata._syntheticdata as _syntheticdata
 
 import numpy as np
+from rotations import *
 
 
 class RtxLidar(BaseSensor):
     """
     Replacement for omni.isaac.sensor.LidarRtx class
     Improvements:
-    - add event listener to get point cloud data at correct time
-    - add option to buffer frames for a full lidar rotation
-    - add option to remove motion distortion from the point cloud
-    - add option to get the point cloud in world frame
+    - connect acquisition callback to stage rendering event instead of update event
+    - add buffering option to buffer frames and return them in a single callback
+    TODO: it only works if simulation rate is the same as rotation rate.
+    For example if lidar is 10Hz and sim render at 30fps the point cloud will fail
     """
 
     def __init__(
@@ -33,18 +36,35 @@ class RtxLidar(BaseSensor):
         translation: Optional[np.ndarray] = None,
         orientation: Optional[np.ndarray] = None,
         config_file_name: Optional[str] = None,
-        transform_to_world: bool = False,
-        no_distortion: bool = False
-    ) -> None:
-        self._current_frames = {}
+        max_queue_size: int = 1
+    ):
+        self._max_queue_size = max_queue_size
         self._config = self._read_config(config_file_name)
         self.rotation_hz = self._config['profile']['scanRateBaseHz']
         self._buffering = False
         self._buffer_cb = None
-        self._transform_to_world = transform_to_world
-        self._no_distortion = no_distortion
-        self._event_sub = None
-        self._message_bus = None
+
+        self._render_product_path = None
+        self._render_event_sub = None
+        self._og_controller = None
+        self._sdg_graph_pipeline = None
+        self._sdg_interface = None
+
+        self._frame_keys = [
+            "frameId",
+            "azimuths",
+            "elevations",
+            "distances",
+            "intensities",
+            "hitPointNormals",
+            "materialIds",
+            "objectIds",
+            "transform",
+            "transformStart",
+        ]
+
+        self._current_frames = {}
+        self._clear_current_frames()
 
         self._create_rtx_lidar_sensor(prim_path, config_file_name)
         BaseSensor.__init__(
@@ -56,12 +76,10 @@ class RtxLidar(BaseSensor):
             orientation=orientation
         )
         self._create_point_cloud_graph()
-        # self._create_event_sender()
-        return
 
     def __del__(self):
-        if self._event_sub is not None:
-            self._event_sub.unsubscribe()
+        if self._render_event_sub is not None:
+            self._render_event_sub.unsubscribe()
 
     def _read_config(self, config_file_name):
         config_path = os.path.abspath(
@@ -79,6 +97,11 @@ class RtxLidar(BaseSensor):
         with open(file_path, 'rt', encoding='utf-8') as f:
             config = json.load(f)
         return config
+
+    def _clear_current_frames(self):
+        keys = self._frame_keys + ['rendering_time', 'xyz']
+        for k in keys:
+            self._current_frames[k] = []
 
     @property
     def config(self):
@@ -98,13 +121,11 @@ class RtxLidar(BaseSensor):
                 "IsaacSensorCreateRtxLidar", path=prim_path, parent=None, config=config_file_name
             )
 
-    def _og_connect(self, n1, a1, n2, a2):
-        og.Controller.connect(f'{n1}.outputs:{a1}', f'{n2}.inputs:{a2}')
-
     def _create_point_cloud_graph(self):
         # create base graph by initializing annotator
         self._render_product = rep.create.render_product(
             self._sensor.GetPath(), resolution=(1, 1))
+        self._render_product_path = self._render_product.path
         self._annotator = rep.AnnotatorRegistry.get_annotator(
             # "RtxSensorCpu" + "IsaacComputeRTXLidarPointCloud"
             "RtxSensorCpu" + "IsaacReadRTXLidarData"
@@ -113,71 +134,66 @@ class RtxLidar(BaseSensor):
         lidar_node = self._annotator.get_node().get_prim_path()
         og.Controller.attribute(
             "inputs:keepOnlyPositiveDistance", lidar_node).set(False)
-
-    def _create_event_sender(self):
-        lidar_node = self._annotator.get_node().get_prim_path()
-        # add event node
-        graph_path = '/'.join(lidar_node.split('/')[:-1])
-        event_node = f'{graph_path}/event_node'
-        event_name = 'rtxLidarReady'
-        og.Controller.create_node(
-            event_node, "omni.graph.action.SendMessageBusEvent")
-        og.Controller.attribute("inputs:eventName", event_node).set(event_name)
-        self._og_connect(lidar_node, "exec", event_node, "execIn")
-        attrs = [("azimuths", "float[]"),
-                 ("elevations", "float[]"),
-                 ("distances", "float[]"),
-                 ("transform", "matrixd[4]"),
-                 ("transformStart", "matrixd[4]"),
-                 ("frameId", "uint64")]
-        for aname, atype in attrs:
-            og.Controller.create_attribute(event_node, aname, atype)
-            self._og_connect(lidar_node, aname, event_node, aname)
-
         self._time_annotator = rep.AnnotatorRegistry.get_annotator(
             "IsaacReadTimes")
-        self._time_annotator.attach([self._render_product])
-        og.Controller.create_attribute(
-            event_node, "rendering_time", "double")
-        time_node = self._time_annotator.get_node().get_prim_path()
-        self._og_connect(time_node, "simulationTime",
-                         event_node, "rendering_time")
+        self._time_annotator.attach([self._render_product_path])
 
-        # set event listener
-        # event_type = carb.events.type_from_string(event_name)
-        # self._message_bus = omni.kit.app.get_app().get_message_bus_event_stream()
-        # self._event_sub = self._message_bus.create_subscription_to_pop_by_type(
-        #     event_type, self._on_lidar_event)
+        # self._print_writer = rep.WriterRegistry.get(
+        #     "WriterIsaacPrintRTXSensorInfo")
+        # self._print_writer.attach([self._render_product_path])
 
-    def _on_lidar_event(self, e):
-        frame = e.payload.get_dict()
-        print('_on_lidar_event: ', self._message_bus.event_count)
-        if _is_empty_frame(frame):
+        # self._render_event_sub = (
+        #     omni.usd.get_context()
+        #     .get_rendering_event_stream()
+        #     .create_subscription_to_pop_by_type(
+        #         int(omni.usd.StageRenderingEventType.NEW_FRAME),
+        #         self._data_acquisition_callback,
+        #         name="my.rtx.lidar.data.acquisition",
+        #         order=1000,
+        #     )
+        # )
+        self._render_event_sub = (
+            omni.usd.get_context()
+            .get_rendering_event_stream()
+            .create_subscription_to_pop(
+                self._data_acquisition_callback
+            )
+        )
+        self._events = []
+
+        self._og_controller = og.Controller()
+        self._sdg_graph_pipeline = "/Render/PostProcess/SDGPipeline"
+        self._sdg_interface = _syntheticdata.acquire_syntheticdata_interface()
+
+    def _data_acquisition_callback(self, event):
+        self._events.append(event)
+        parsed_payload = self._sdg_interface.parse_rendered_simulation_event(
+            event.payload["product_path_handle"], event.payload["results"]
+        )
+
+        if parsed_payload[0] != self._render_product_path:
             return
 
-        frame['azimuths'] = np.array(frame['azimuths'])
-        frame['elevations'] = np.array(frame['elevations'])
-        frame['distances'] = np.array(frame['distances'])
-        frame['transform'] = np.array(frame['transform']).reshape(4, 4).T
-        frame['transformStart'] = np.array(
-            frame['transformStart']).reshape(4, 4).T
-        frame['xyz'] = _calculate_xyz(frame, degrees=True)
+        self._og_controller.evaluate_sync(
+            graph_id=self._sdg_graph_pipeline)
 
-        if self._no_distortion:
-            transforms = np.array(
-                [frame['transformStart'], frame['transform']])
-            frame['xyz'] = undistort(frame['xyz'], transforms)
-            if not self._transform_to_world:
-                frame['xyz'] = _to_sensor(
-                    frame['xyz'], frame['transform'])
+        lidar_data = self._annotator.get_data()
+        if len(lidar_data["azimuths"]) == 0:
+            print("skip empty frame")
+            return
 
-        if not self._no_distortion and self._transform_to_world:
-            frame['xyz'] = _to_world(frame['xyz'], frame['transform'])
+        time_data = self._time_annotator.get_data()
+        rendering_time = time_data['simulationTime']
+        self._current_frames['rendering_time'].append(rendering_time)
+        for key in self._frame_keys:
+            if key in ['transform', 'transformStart']:
+                transform = lidar_data[key].reshape(4, 4).T
+                self._current_frames[key].append(transform)
+            else:
+                self._current_frames[key].append(lidar_data[key])
 
-        for key in frame.keys():
-            if key not in self._current_frames:
-                self._current_frames[key] = []
-            self._current_frames[key].append(frame[key])
+        point_cloud = _calculate_xyz(lidar_data, degrees=True)
+        self._current_frames['xyz'].append(point_cloud)
 
         if self._buffering:
             self._check_buffer()
@@ -192,7 +208,7 @@ class RtxLidar(BaseSensor):
         if steps_per_second % frames_per_second != 0:
             raise ValueError("frame time must be a multiple of render step")
         steps_per_frame = steps_per_second // frames_per_second
-        if len(self._current_frames) >= steps_per_frame:
+        if len(self._current_frames['rendering_time']) >= steps_per_frame:
             result = {}
             for key, _ in self._current_frames.items():
                 result[key] = self._current_frames[key][:steps_per_frame]
@@ -215,22 +231,6 @@ class RtxLidar(BaseSensor):
         """
         self._buffering = buffering
         self._buffer_cb = buffer_cb
-
-    def set_transform_to_world(self, transform_to_world):
-        """
-        get the point cloud in world frame
-        """
-        self._transform_to_world = transform_to_world
-
-    def set_no_distortion(self, no_distortion):
-        """
-        remove motion distortion from the point cloud
-        """
-        self._no_distortion = no_distortion
-
-
-def _is_empty_frame(frame):
-    return len(frame['azimuths']) == 0
 
 
 def _linear_interpolate(xp, yp, x):
@@ -278,15 +278,22 @@ def undistort(pcd, transforms):
     the interpolation and simply move it to world frame
     Args:
         pcd: point cloud in sensor frame
-        transforms: transform at the beginning and end of the frame
+        transforms: [transformStart, transformEnd] pose of lidar when capture first and last point.
     Return:
-        undistorted point cloud in world frame.
+        undistorted point cloud in sensor frame (transformEnd)
     """
     if np.allclose(transforms[0], transforms[1]):
-        return _to_world(pcd, transforms[0])
+        return pcd
 
     positions = np.array([t[:3, 3] for t in transforms])
     rotations = np.array([t[:3, :3] for t in transforms])
+    angles = np.array([matrix_to_euler_angles(r) for r in rotations])
+    pi = np.pi
+    angles_changes = angles[1] - angles[0]
+    # assuming during one scan, the frame rotate in the shorter direction
+    angles_changes[angles_changes > pi] -= 2*pi
+    angles_changes[angles_changes < -pi] += 2*pi
+    angles[1] = angles[0] + angles_changes
     pcd1 = pcd[:len(pcd)//2]
     pcd2 = pcd[len(pcd)//2:]
     timestamps = np.array([0, 1])
@@ -294,10 +301,15 @@ def undistort(pcd, transforms):
 
     pos_interpolated = _linear_interpolate(
         timestamps, positions, times)
-    rot_interpolated = _linear_interpolate(
-        timestamps, rotations, times)
+    angles_interpolated = _linear_interpolate(
+        timestamps, angles, times)
+    rot_interpolated = np.array([euler_to_rot_matrix(a)
+                                for a in angles_interpolated])
+    # rot_interpolated = _linear_interpolate(
+    #     timestamps, rotations, times)
     pcd1 = np.expand_dims(pcd1, axis=-1)
     pcd1_undistorted = np.squeeze(
         np.matmul(rot_interpolated, pcd1)) + pos_interpolated
-    pcd2_undistorted = _to_world(pcd2, transforms[-1])
+    pcd1_undistorted = _to_sensor(pcd1_undistorted, transforms[-1])
+    pcd2_undistorted = pcd2
     return np.concatenate([pcd1_undistorted, pcd2_undistorted])
